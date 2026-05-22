c3b2d3809020737123556db517787dbb737bf3fd
efromanillos <e.f.romanillos@gmail.com>
2026-05-23 01:17:22 +0200

    feat: configurados estilos globales y variables HSL premium

--- src/services/chatbotService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export default {
-  async sendMessage(messageText) {
-    const response = await api.post('/chatbot', { message: messageText });
-    return response.data; // { reply: "..." }
-  }
-};
